cfe375e36b93f09bbb244f3def5a2d499de30ec1
bzsol <bereczzsolt2000@gmail.com>
2023-10-09 00:06:32 +0200

    Added tiles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, LayersControl } from "react-leaflet";
 
 const App = () => {
   const position = [51.505, -0.09];
@@ -8,12 +8,24 @@ const App = () => {
       center={position}
       zoom={13}
       scrollWheelZoom={true}
-      style={{ minHeight: "100vh", minWidth: "100vw" }}
+      style={{ minHeight: "100vh", minWidth: "10vw" }}
     >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+       <LayersControl position="topright">
+        <LayersControl.BaseLayer checked name="OpenStreetMap">
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Satellite">
+          <TileLayer
+        url='https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
+        maxZoom={20}
+        subdomains={['mt1', 'mt2', 'mt3']}
       />
+          </LayersControl.BaseLayer>
+          </LayersControl>
+      
       <Marker position={position}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
